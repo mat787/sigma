3d4a5b4de31e301618b22b851010ec4268241f01
Matylda Drapich <01197793@pw.edu.pl>
2024-12-19 19:45:34 +0000

    Y

--- src/gauss.c
+++ src/gauss.c
@@ -1,24 +1,64 @@
 #include "gauss.h"
+#include <stdio.h>
+#
 
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
 
-int mnozenie(Matrix *mat, double a)
+int eliminate(Matrix *mat, Matrix *b)
 {
-	int i, j;
-for (i = 0; i<mat->r; i++) {
-                for (j = 0; j < mat->c; j++) {
-                       mat->data[i][j]=mat->data[i][j]*a;
+	int i,j,k,c;
+	Matrix* workMat;
 
+	if(mat->r != mat->c)
+	{
+		printf("Macierz nie jest kwadratowa");
+		return -1;
+	}
+	int n = mat->r;
+// create augmented matrix
+	workMat = createMatrix(n, n+1);
+    	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+                        workMat->data[i][j] = mat->data[i][j];
+                }
+        }
+        for (i = 0; i < n; i++)
+        {
+          workMat->data[i][n] = *b->data[i];
+        }
+  
+printf("start n=%d\n", n);
+	for(i = 0; i < n-1; i++)
+	{	
+        	for(j = 1; j <= n; j++)
+        	{
+printf("i=%d, j=%d\n", i, j);
 
-}
-int eliminate(Matrix *mat, Matrix *b){
-  if(mat->r != mat->c)
-	  return -1;
-  int n=mat->r;
+//            		if(j > i)
+            		{
+if (mat->data[i][i] == 0)
+ printf("dzielenie przez 0\n");
+	 			c = workMat->data[j][i]/workMat->data[i][i];
+               			for(k=0; k<=n; k++)
+               			{	
+printf("j=%d, k=%d\n", j,k);
+                    			workMat->data[j][k] = workMat->data[j][k]-c*workMat->data[i][k];
+                		}
+            		}
+        	}
+    	}
 
-		return 0;
+if (mat->data[n-1][n-1] == 0)                               
+ printf("dzielenie przez 0\n");
+
+	*b->data[n-1] = workMat->data[n-1][n]/workMat->data[n-1][n-1];
+
+	return 0;
+
 }
 
