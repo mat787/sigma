18b1f5121e915893771f7a5044f35cae53baa961
Matylda Drapich <01197793@pw.edu.pl>
2024-12-19 15:57:16 +0000

    test

--- src/gauss.c
+++ src/gauss.c
@@ -4,10 +4,20 @@
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
+
+int mnozenie(Matrix *mat, double a)
+{
+	int i, j;
+for (i = 0; i<mat->r; i++) {
+                for (j = 0; j < mat->c; j++) {
+                       mat->data[i][j]=mat->data[i][j]*a;
+
+
+}
 int eliminate(Matrix *mat, Matrix *b){
-    /**
-  	 * Tutaj należy umieścić właściwą implemntację.
-		 */
+  if(mat->r != mat->c)
+	  return -1;
+  int n=mat->r;
 
 		return 0;
 }
