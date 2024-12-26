aa651981ae7337ebc9819452abcf5c8008bac2a9
Julia Sz <julaszosz@gmail.com>
2024-12-27 00:15:12 +0100

    dzialajacy gauss (jeszcze bez uwzglednienia zer)

--- src/gauss.c
+++ src/gauss.c
@@ -1,6 +1,6 @@
 #include "gauss.h"
 #include <stdio.h>
-#
+#include <math.h>
 
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
@@ -9,56 +9,66 @@
 
 int eliminate(Matrix *mat, Matrix *b)
 {
-	int i,j,k,c;
-	Matrix* workMat;
+        int i,j,k,c;
+        Matrix* workMat;
 
-	if(mat->r != mat->c)
-	{
-		printf("Macierz nie jest kwadratowa");
-		return -1;
-	}
-	int n = mat->r;
-// create augmented matrix
-	workMat = createMatrix(n, n+1);
-    	for (i = 0; i < n; i++)
-	{
-		for (j = 0; j < n; j++)
-		{
+        if(mat->r != mat->c)
+        {
+                printf("Macierz nie jest kwadratowa");
+                return -1;
+        }
+        int n = mat->r;
+
+        workMat = createMatrix(n, n+1);
+
+//przepisanie mat do workmat
+        for (i = 0; i < n; i++)
+        {
+                for (j = 0; j <= n; j++)
+                {
                         workMat->data[i][j] = mat->data[i][j];
                 }
         }
+//przepisanie b do workmat
         for (i = 0; i < n; i++)
         {
-          workMat->data[i][n] = *b->data[i];
+                workMat->data[i][n] = b->data[i][0];
         }
-  
-printf("start n=%d\n", n);
-	for(i = 0; i < n-1; i++)
-	{	
-        	for(j = 1; j <= n; j++)
-        	{
-printf("i=%d, j=%d\n", i, j);
 
-//            		if(j > i)
-            		{
-if (mat->data[i][i] == 0)
- printf("dzielenie przez 0\n");
-	 			c = workMat->data[j][i]/workMat->data[i][i];
-               			for(k=0; k<=n; k++)
-               			{	
-printf("j=%d, k=%d\n", j,k);
-                    			workMat->data[j][k] = workMat->data[j][k]-c*workMat->data[i][k];
-                		}
-            		}
-        	}
-    	}
 
-if (mat->data[n-1][n-1] == 0)                               
- printf("dzielenie przez 0\n");
+        //double *temp = 0;
+//eliminacja gaussa
+        for(j = 0; j < n; j++)
+        {
+                for(i = 0; i < n; i++)
+                {
+                        if(i>j)
+                        {
+                                c = workMat->data[i][j]/workMat->data[j][j];
 
-	*b->data[n-1] = workMat->data[n-1][n]/workMat->data[n-1][n-1];
+                                for(k=0; k<=n; k++)
+                                {
+                                        workMat->data[i][k] = workMat->data[i][k]-c*workMat->data[j][k];
+                                }
+                        }
+                }
+        }
 
-	return 0;
+//przepisanie workmat do mat
+        for (i = 0; i < n; i++)
+        {
+                for (j = 0; j < n; j++)
+                {
+                        mat->data[i][j] = workMat->data[i][j];
+                }
+        }
 
+//przepisanie workmat do b
+        for (i = 0; i < n; i++)
+        {
+                b->data[i][0] = workMat->data[i][n];
+        }
+
+        return 0;
 }
 
